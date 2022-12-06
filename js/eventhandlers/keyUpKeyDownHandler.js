1f1d0c5958daf29775736dc06bb3aeb7a4c77073
rohitm53 <rohitmanohar53@gmail.com>
2022-12-07 01:57:35 +0530

    Added event handler

--- js/eventhandlers/keyUpKeyDownHandler.js
+++ js/eventhandlers/keyUpKeyDownHandler.js
@@ -4,13 +4,20 @@ $(function () {
   //     console.log(event.which);
   //   });
 
+  var ARROW_RIGHT = 39;
+  var ARROW_LEFT = 37;
+
   var blueBox = $(".blue-box");
   $("html").on("keydown", function (event) {
     console.log(event.which);
-    if (event.which === 39) {
-      blueBox.css("margin-left", "+=10px");
-    } else if (event.which === 37) {
-      blueBox.css("margin-left", "-=10px");
+    if (event.which === ARROW_RIGHT) {
+      blueBox.stop().animate({
+        marginLeft: "+=10px",
+      });
+    } else if (event.which === ARROW_LEFT) {
+      blueBox.stop().animate({
+        marginLeft: "-=10px",
+      });
     }
   });
 });
